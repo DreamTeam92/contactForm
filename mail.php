2092b35f01c0b8e9543de47e0c8bc356c189e958
VIKTOR ARVA-TOTH <45672316+DreamTeam92@users.noreply.github.com>
2022-12-16 16:48:13 +0000

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,42 +1,36 @@
-<?php  
- 
-if(isset($_POST['submit'])) {
- $mailto = "dreamteam92@hotmail.co.uk";  //My email address
- //getting customer data
- $name = $_POST['name']; //getting customer name
- $fromEmail = $_POST['email']; //getting customer email
- $phone = $_POST['tel']; //getting customer Phome number
- $subject = $_POST['subject']; //getting subject line from client
- $subject2 = "Confirmation: Message was submitted successfully | HMA WebDesign"; // For customer confirmation
- 
- //Email body I will receive
- $message = "Cleint Name: " . $name . "\n"
- . "Phone Number: " . $phone . "\n\n"
- . "Client Message: " . "\n" . $_POST['message'];
- 
- //Message for client confirmation
- $message2 = "Dear" . $name . "\n"
- . "Thank you for contacting us. We will get back to you shortly!" . "\n\n"
- . "You submitted the following message: " . "\n" . $_POST['message'] . "\n\n"
- . "Regards," . "\n" . "- HMA WebDesign";
- 
- //Email headers
- $headers = "From: " . $fromEmail; // Client email, I will receive
- $headers2 = "From: " . $mailto; // This will receive client
- 
- //PHP mailer function
- 
-  $result1 = mail($mailto, $subject, $message, $headers); // This email sent to My address
-  $result2 = mail($fromEmail, $subject2, $message2, $headers2); //This confirmation email to client
- 
-  //Checking if Mails sent successfully
- 
-  if ($result1 && $result2) {
-    $success = "Your Message was sent Successfully!";
-  } else {
-    $failed = "Sorry! Message was not sent, Try again Later.";
-  }
- 
-}
- 
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+$recipient = "dreamteam92@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
+
+echo'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
+        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
+        
+    </div>
+</body>
+</html>
+';
+
+
 ?>
