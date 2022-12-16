034c9c29883ff341700610aebc601c93bf444b9a
VIKTOR ARVA-TOTH <45672316+DreamTeam92@users.noreply.github.com>
2022-12-16 16:46:40 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,28 @@
-<?php include 'sentMail.php';?>
- 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <div class="container">  
-    <form id="contact" action="" method="post">
-      <h3>Quick Contact</h3>
-      <h4>Contact us today, and get reply with in 24 hours!</h4>
- 
-      <fieldset>
-        <input placeholder="Your name" name="name" type="text" tabindex="1" autofocus>
-      </fieldset>
-      <fieldset>
-        <input placeholder="Your Email Address" name="email" type="email" tabindex="2">
-      </fieldset>
-      <fieldset>
-        <input placeholder="Your Phone Number" name="tel" type="tel" tabindex="3" >
-      </fieldset>
-      <fieldset>
-        <input placeholder="Type your subject line" type="text" name="subject" tabindex="4">
-      </fieldset>
-      <fieldset>
-        <textarea name="message" placeholder="Type your Message Details Here..." tabindex="5"></textarea>
-      </fieldset>
-      <div>
-         <p class="success"> <?php echo $success;  ?></p>
-         <p class="failed"> <?php echo $failed;  ?></p>
-      </div>
-      <fieldset>
-        <button type="submit" name="submit" id="contact-submit" data-submit="...Sending">Submit Now</button>
-      </fieldset>
-    </form>
-    
-     
-  </div>
+    <div class="container">
+        <h1>Contact Me</h1>
+        <p>Feel free to contact us and we will get back to you ass sson as we can.</p>
+        <form action="mail.php" method="POST">
+            <label for="name">Name:</label>
+            <input type="text" name="name" id="name">
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+            <label for="subject">Subject:</label>
+            <input type="text" name="subject" id="subject">
+            <label for="message">Message</label>
+            <textarea name="message" cols="30" rows="10"></textarea>
+            <input type="submit" value="Send">
+        </form>
+    </div>
 </body>
 </html>
